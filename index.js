c44eec8efa20de3866b8ed76511ec220ece86746
Anastasia Nedelina <anastasy45@ya.ru>
2020-08-20 14:14:45 +0500

    Task 1 about movies was finished

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const readline = require('readline');
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
 
-const rl = readline.createInterface(process.stdin, process.stdout);
-rl.on('line', (line) => {
-    // Введенная строка в переменной line, тут можно написать решение
-    console.log(String(result));
-    rl.close();
-    return;
-}).on('close', () => process.exit(0));
+const a = prompt("Один из последних просмотреннвх фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотреннвх фильмов?", ""),
+      d = prompt("На сколько оцените его?", "");
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d; 
+
+      console.log(personalMovieDB);
